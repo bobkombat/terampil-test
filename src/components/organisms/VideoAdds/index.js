27148f1eba52cea91713b70475c5e3bb5ab41b4b
aldamtrisena <aldam3sena@gmail.com>
2021-01-25 15:28:06 +0700

    jdssj

--- src/components/organisms/VideoAdds/index.js
+++ src/components/organisms/VideoAdds/index.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import { Container, VideoPlayer } from './styled';
 
 const videoPlayer = {
-  src:
-    'https://cdn.videvo.net/videvo_files/video/premium/2019-08/small_watermarked/170216B_147_BoyPlayingPiano_1080p_preview.webm',
+  src: 'https://d2jr0bw5daaf2g.cloudfront.net/sample-mp4-file.mp4',
   thumbnail:
     'https://images.unsplash.com/photo-1474314881477-04c4aac40a0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
 };
